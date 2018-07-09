98eb67e30157c14e9ce32a65ce65ffee90c972ca
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-07-09 19:11:27 +0600

    splice and slice is completed

---
+++ Array Methods/splice-slice.js
@@ -0,0 +1,12 @@
+// splice 
+
+const arr = ['santo', 'rakib', 'emon', 'suhan'];
+
+arr1 = arr.splice(1,0,'nazrul', 'sumi');
+console.log(arr);
+
+
+// slice
+arr2 = arr.slice(1,2);
+console.log(arr2);
+
